3d8f7e456e70dca6e1548ca6a5cdaad1d29784dd
6Morpheus6 <wanton-decay@gmx.net>
2025-11-09 19:50:20 +0100

    update

--- torch.js
+++ torch.js
@@ -9,7 +9,7 @@ module.exports = {
         "path": "{{args && args.path ? args.path : '.'}}",
         "message": [
           "uv pip install torch==2.7.0 torchvision==0.22.0 torchaudio==2.7.0 {{args && args.xformers ? 'xformers==0.0.30' : ''}} --index-url https://download.pytorch.org/whl/cu128",
-          "{{args && args.triton ? 'triton-windows==3.3.1.post19' : ''}}",
+          "{{args && args.triton ? 'uv pip install triton-windows==3.3.1.post19' : ''}}",
           "uv pip install torchao"
         ]
       }
